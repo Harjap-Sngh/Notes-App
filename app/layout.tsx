bd5f167b54a7e29d2d39c8adf1d832e9a43c5c64
Harjap Singh <157319482+Harjap-Sngh@users.noreply.github.com>
2025-03-07 09:15:32 -0700

    Starting on making a landing page 37:14

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import db from "@/lib/supabase/db";
-
+import { ThemeProvider } from "@/lib/providers/next-theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,11 +26,13 @@ export default function RootLayout({
 }>) {
   console.log(db);
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider defaultTheme="dark" attribute="class" enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
