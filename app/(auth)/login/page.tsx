950d969c5aea7f57277dc634124253f83c8260be
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-04-15 16:32:49 -0600

    creating dashboard

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormSchema } from '@/lib/types';
 import {
   Form,
   FormControl,
@@ -11,115 +12,129 @@ import {
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form";
-import Link from "next/link";
-import Image from "next/image";
-import Logo from "../../../public/cypresslogo.svg";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import Loader from "@/components/Loader";
+} from '@/components/ui/form';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../../../../public/cypresslogo.svg';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import Loader from '@/components/global/Loader';
+import { Separator } from '@/components/ui/separator';
+import { actionLoginUser } from '@/lib/server-actions/auth-actions';
 
 const LoginPage = () => {
   const router = useRouter();
-  const [submitError, setSubmitError] = useState("");
+  const [submitError, setSubmitError] = useState('');
 
-  const form = useForm({
-    mode: "onChange",
-    resolver: zodResolver(
-      z.object({
-        email: z.string().email("Invalid email address"),
-        password: z.string().min(1, "Password must be at least 6 characters"),
-      })
-    ),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: 'onChange',
+    resolver: zodResolver(FormSchema),
+    defaultValues: { email: '', password: '' },
   });
 
   const isLoading = form.formState.isSubmitting;
 
-  const onSubmit = async (formData: any) => {};
+  const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+    formData
+  ) => {
+    const { error } = await actionLoginUser(formData);
+    if (error) {
+      form.reset();
+      setSubmitError(error.message);
+    }
+    router.replace('/dashboard');
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen px-4 py-6 space-y-6 bg-background/50 dark:bg-background/80 backdrop-blur-sm">
-      <Form {...form}>
-        <form
-          onChange={() => {
-            if (submitError) setSubmitError("");
-          }}
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
-        >
-          <Link
-            href="/"
-            className="
+    <Form {...form}>
+      <form
+        onChange={() => {
+          if (submitError) setSubmitError('');
+        }}
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full sm:justify-center sm:w-[400px] space-y-6 flex flex-col"
+      >
+        <Link
+          href="/"
+          className="
           w-full
           flex
           justify-left
           items-center"
-          >
-            <Image src={Logo} alt="cypress Logo" width={50} height={50} />
-            <span
-              className="font-semibold
+        >
+          <Image
+            src={Logo}
+            alt="cypress Logo"
+            width={50}
+            height={50}
+          />
+          <span
+            className="font-semibold
           dark:text-white text-4xl first-letter:ml-2"
-            >
-              cypress.
-            </span>
-          </Link>
-          <FormDescription
-            className="
-        text-foreground/60"
           >
-            An all-In-One Collaboration and Productivity Platform
-          </FormDescription>
-          <FormField
-            disabled={isLoading}
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input type="email" placeholder="Email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            disabled={isLoading}
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input type="password" placeholder="Password" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {submitError && <FormMessage>{submitError}</FormMessage>}
-          <Button
-            type="submit"
-            className="w-full p-6"
-            size="lg"
-            disabled={isLoading}
-          >
-            {!isLoading ? "Login" : <Loader />}
-          </Button>
-          <span className="self-container w-[100%] text-center text-foreground/60">
-            Dont have an account?{" "}
-            <Link
-              href="/signup"
-              className="text-primary hover:text-primary-purple-600"
-            >
-              Sign Up
-            </Link>
+            cypress.
           </span>
-        </form>
-      </Form>
-    </div>
+        </Link>
+        <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <Button
+          type="submit"
+          className="w-full p-6"
+          size="lg"
+          disabled={isLoading}
+        >
+          {!isLoading ? 'Login' : <Loader />}
+        </Button>
+        <span className="self-container">
+          Dont have an account?{' '}
+          <Link
+            href="/signup"
+            className="text-primary"
+          >
+            Sign Up
+          </Link>
+        </span>
+      </form>
+    </Form>
   );
 };
 
